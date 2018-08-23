8fa5b6bde30a05d9ec79306b823c350554c76866
Vishal Kumar <sohalvishal.Kumar@gmail.com>
2018-08-23 14:28:16 +0530

    profile images updates

--- src/scenes/Profile.js
+++ src/scenes/Profile.js
@@ -886,6 +886,10 @@ class Profile extends Component {
         </View>
       );
     }
+    let { user_photo_thumb } = this.state;
+		if (user_photo_thumb.includes('https://puffy-uploadsresized.s3.amazonaws.com/resized-uploads')){
+			user_photo_thumb = user_photo_thumb.replace('https://puffy-uploadsresized.s3.amazonaws.com/resized-uploads', 'https://s3-us-west-2.amazonaws.com/puffy.assets/uploadsresized/resized-uploads');
+    }
     return (
       <View style={styles.container}>
         <Header
@@ -901,7 +905,7 @@ class Profile extends Component {
           <View style={styles.profileContainer}>
             <View style={styles.profileLeft}>
               <TouchableOpacity onPress={this.setVisibleToTrue}>
-                <CachedImage style={styles.userPhoto} source={{ uri: this.state.user_photo_thumb, cache: "force-cache" }} />
+                <CachedImage style={styles.userPhoto} source={{ uri: user_photo_thumb, cache: "force-cache" }} />
               </TouchableOpacity> 
             </View>
             <View style={styles.profileRight}>
